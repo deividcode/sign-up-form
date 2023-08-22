0bd7714afb5968ef6f328e4e1618f3315edbe39e
Jose Quintero <quinteromda@gmail.com>
2023-08-22 18:24:09 -0400

    Form Sign-up Finish

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const showMsjError = 'show';
 const hiddenMsjError = 'hidden';
 
+let elModal = document.querySelector('.modal');
+
 const formSignUp = document.querySelector('.form-signUp');
 let inputPassword = document.querySelector("[name='password']");
 let inputConfirmPassword = document.querySelector("[name='confirmPassword']");
@@ -8,27 +10,33 @@ let elMsjError = document.querySelector('.msjError');
 let formEl = document.forms['form'];
 
 
-
 // Event Listener
 formSignUp.addEventListener('submit', getData);
 inputConfirmPassword.addEventListener('input', samePassword);
 
+
 function getData(e){
   e.preventDefault();
 
-  let [name, lastname, email, phoneNumber, password] = formEl.elements;
+  let [name, lastName, email, phoneNumber, password] = formEl.elements;
   
-  const userData = {
-    'name': name.value,
-    'lastname': lastname.value,
-    'email': email.value,
-    'phoneNumber': phoneNumber.value,
-    'password': password.value
-  }
+  if(name.value !== null, lastName.value !== null, 
+    email.value !== null, phoneNumber.value !== null, 
+    password.value !== null){
 
-  console.log(userData);
+      const userData = {
+        'name': name.value ,
+        'lastName': lastName.value,
+        'email': email.value,
+        'phoneNumber': phoneNumber.value,
+        'password': password.value
+      }
+      
+      formEl.reset();
 
-  msjWelcome(userData);  
+      showModal(userData.name, userData.lastName);  
+    }  
+
 }
 
 function samePassword(params) {
@@ -53,5 +61,34 @@ function msjErrorForm(msj, type){
     inputConfirmPassword.style.borderBottom = '2px #0000004f solid';     
     elMsjError.style.display = 'none';
   }
+
+}
+
+function showModal(name, lastName) { 
+
+  let show = `        
+    <div class="content-modal">
+      <h2>Welcome ${name + ' ' + lastName}</h2>
+      <p>Already a member of <span class="highlight">Music L.A.</span> Now you can enjoy the music you want</p>
+
+      <div class="modal-buttons">
+        <button class="form-signUp-button">Empezar</button>
+        <button class="form-signUp-button button-gray close-modal">Cerrar</button>
+      </div>
+
+    </div>    
+  `;
+    
+  elModal.style.display = 'grid';
+  elModal.insertAdjacentHTML("beforeend", show);
+
+  let elContentModal = document.querySelector('.content-modal');
+  let elCloseModal = document.querySelector('.close-modal');
+  
+  
+  elCloseModal.addEventListener('click', () =>{
+    elModal.style.display = 'none';
+    elModal.removeChild(elContentModal);
+  });
 
 }
